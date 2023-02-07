4d2c9996016682347eae1162a54fa2a62df97aeb
Ben Grinthal <bgrinthal@gmail.com>
2023-02-07 09:15:46 -0500

    connected category data from controller to view

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        // implimentation of connection strings and tables to retreive the data
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            // sets local variable to db
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            // connect to data, then category, then make list 
+            // IEnumerable<Category> defines a collect.  If use just "var" need _db.Categories.ToList()
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            // Pass to view
+            return View(objCategoryList);
         }
     }
 }
