5fa36064ad74367a369b4a9adf3891bb85aa9e2b
Ben Grinthal <bgrinthal@gmail.com>
2023-02-09 12:48:13 -0500

    fixed edit category to work properly and update

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -52,5 +52,48 @@ namespace BulkyBookWeb.Controllers
             }
             return View(obj);
         }
+
+        // GET
+        public IActionResult Edit(int? id)
+        {
+            if(id== null || id==0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            // other methods to find correct id.  These will return dafault if nothing is found (instead of throwing error)
+            // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            // checks if name is equal to display order, and displays a custom error if true
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                // calling the error "name" adds it to the model property of "Name"
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            // checks if entered info aligns with checks (such as [Required]) in the model
+            if (ModelState.IsValid)
+            {
+                // connects to db, and adds category object
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                // redirects to Index method in category controller
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
