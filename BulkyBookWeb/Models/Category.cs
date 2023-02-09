0d69f4d2f67e74fd04da70c82eadbca014df67c6
Ben Grinthal <bgrinthal@gmail.com>
2023-02-09 12:19:41 -0500

    added more data annotations restrictions in model

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace BulkyBookWeb.Models
 {
@@ -8,6 +9,10 @@ namespace BulkyBookWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        // Data Annotation that changes the name of DisplayOrder that displays
+        [DisplayName("Display Order")]
+        // Only allows numbers 1 - 100, with custom error message
+        [Range(1,100,ErrorMessage="Display Order must be between 1 and 100 only")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
