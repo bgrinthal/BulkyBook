3899dbc7e9dc5f176a216960eab3b5b2239db2dc
Ben Grinthal <bgrinthal@gmail.com>
2023-02-10 13:31:11 -0500

    added delete GET/POST to remove from database

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -95,5 +95,43 @@ namespace BulkyBookWeb.Controllers
             }
             return View(obj);
         }
+
+        // GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            // other methods to find correct id.  These will return dafault if nothing is found (instead of throwing error)
+            // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+                // connects to db, and adds category object
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                // redirects to Index method in category controller
+                return RedirectToAction("Index");
+            return View(obj);
+        }
     }
 }
