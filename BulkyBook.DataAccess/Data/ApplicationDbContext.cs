72d2e6bfe8302f9cebaefc1308eb877a22beb026
Ben Grinthal <bgrinthal@gmail.com>
2023-02-13 10:22:18 -0500

    added seperate projects to handle data, models, and overall utility.  Modified code to find new locations of them

---
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.DataAccess;
+
+public class ApplicationDbContext : DbContext
+{
+    // establish connection with entity framework and pass options
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    // create a Db Set
+    // DbSet<model name> TableName
+    // This will create a category table with the name Categories with 4 column from the category model
+    public DbSet<Category> Categories { get; set; }
+}
